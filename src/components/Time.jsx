6a3cf2d5dd3904353947ae263f85139a7d397d27
ahmed <ahmed.codex1@gmail.com>
2026-04-11 19:34:05 +0200

    update src

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
-const Time = ({time , name}) => {
+const Time = ({ time, name }) => {
   return (
     <div>
-        <div className='flex items-center justify-around text-xl p-3 bg-[#e2ac93]/75 rounded-lg '>
-            <h2>{name}</h2>
-            <h3>{time}</h3>
-        </div>
+      <div className="flex items-center justify-around text-xl  p-3 bg-[#e2ac93]/75 rounded-lg ">
+        <h2>{name}</h2>
+        <h3>{time}</h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Time
+export default Time;
